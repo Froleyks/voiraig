f605cd2fabe74fb7a4ddf3f54e5b03501bc134d7
froleyks <nfroleyks@gmail.com>
2024-07-31 09:56:04 +0200

    exit message

--- src/voiraig.cpp
+++ src/voiraig.cpp
@@ -15,9 +15,11 @@ int main(int argc, char *argv[]) {
   const bool bug = ic3(*model, cex);
   if (bug) {
     write_witness(*model, cex, options.witness);
+    L0 << "exit 10\n";
     return 10;
   } else {
     write_witness(*model, options.witness);
+    L0 << "exit 20\n";
     return 20;
   }
 }
