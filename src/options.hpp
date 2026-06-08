fec086cf28642a79f09cf7f361e247ab6f0929e3
froleyks <nfroleyks@gmail.com>
2026-06-08 11:53:20 +0200

    enable all safety engines

--- src/options.hpp
+++ src/options.hpp
@@ -15,10 +15,10 @@
   OPTION(bool,     backward_flipping,   1, 0, 1, "use CaDiCaL flipping to sparsify backward traces") \
   OPTION(bool,     backward_simulation, 1, 0, 1, "use ternary simulation to sparsify backward traces") \
   OPTION(bool,     certificate, 1, 0, 1, "produce witness circuit") \
-  OPTION(unsigned, liveness,    0, 0, 2, "liveness engine (0=k,1=lts,2=rlive)") \
+  OPTION(unsigned, liveness,    0, 0, 2, "liveness engine (0=k, 1=lts, 2=rlive)") \
   LOGOPT(bool,     location,    1, 0, 1, "use location for logging") \
   OPTION(unsigned, paths,       2, 0, 2, "type of simple path constrains") \
-  OPTION(unsigned, safety,      2, 2, 2, "safety engine (0=ic3,1=kind,2=backward)") \
+  OPTION(unsigned, safety,      2, 0, 2, "safety engine (0=ic3, 1=kind, 2=backward)") \
   OPTION(bool,     stabilize,   1, 0, 1, "enable stabilizer extraction in k-liveness") \
   OPTION(bool,     trace,       1, 0, 1, "produce cex trace") \
   OPTION(bool,     unique,      0, 0, 1, "always use unique kind witness construction") \
